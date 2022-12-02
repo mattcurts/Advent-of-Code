bc9952123366bba770199a17c07848663a681e00
Matthew <matthewcurtis@uvic.ca>
2022-12-02 04:07:49 -0800

    Init

---
+++ Python/Day 1/main.py
@@ -0,0 +1,19 @@
+f = open("text.txt","r")
+ElfCount = 1
+sum = 0
+lines = f.read().split('\n')
+Elves =[]
+for line in lines:
+    
+    if (line.removesuffix('\n') == ''):
+        print("Elf #",ElfCount," amount",sum)
+        Elves.append(sum)
+        ElfCount+=1
+        sum = 0
+    else:
+        sum = sum + int(line)
+
+Elves.sort(reverse=True)
+result = Elves[0]+ Elves[1]+ Elves[2]
+print(result)
+f.close()
