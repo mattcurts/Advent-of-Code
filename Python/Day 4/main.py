18e63dc5ba0581f9bc5c8f5c46c6c21630171e4f
Matthew <matthewcurtis@uvic.ca>
2022-12-03 22:46:24 -0800

    Day 4 part 1/2

--- Python/Day 4/main.py
+++ Python/Day 4/main.py
@@ -1,17 +1,40 @@
-def pointScore():
-    pass
+import re
 
 
 def part1(lines):
-    pass
+    pairs = 0
+    for line in lines:
+        elf1Start, elf1End, elf2Start, elf2End = map(int, re.split(",|-", line.strip()))
+        if elf1Start <= elf2Start and elf1End >= elf2End:
+            # elf 2 starts after elf 1 and elf 1 ends before elf 2
+            pairs = pairs + 1
+        elif elf2Start <= elf1Start and elf2End >= elf1End:
+            # elf 1 starts after elf 2 and elf 2 ends after elf 1
+            pairs = pairs + 1
+
+    print("Pairs =", pairs)
 
 
 def part2(lines):
-    pass
+    pairs = 0
+    for line in lines:
+        elf1Start, elf1End, elf2Start, elf2End = map(int, re.split(",|-", line.strip()))
+        if elf1Start <= elf2Start and elf1End >= elf2End:
+            # elf 2 starts after elf 1 and elf 1 ends before elf 2
+            # elf 2 is a subset of elf 1
+            pairs = pairs + 1
+        elif elf2Start <= elf1Start and elf2End >= elf1End:
+            # elf 1 starts after elf 2 and elf 2 ends after elf 1
+            # elf 1 is a subset of elf 2
+            pairs = pairs + 1
+        elif elf2Start <= elf1End and elf1Start <= elf2End:
+            pairs = pairs + 1
+
+    print("Pairs =", pairs)
 
 
 def main():
-    f = open("Python/Day 4/input.txt", "r")
+    f = open("Python/Day 4/test.txt", "r")
     lines = f.readlines()
     part1(lines)
     part2(lines)
