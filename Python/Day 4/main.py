d1f68096159a91d999f19ffc081cfb9285b705b6
Matthew <matthewcurtis@uvic.ca>
2022-12-03 20:52:47 -0800

    cleaned up Day 2, Set up Day 4

---
+++ Python/Day 4/main.py
@@ -0,0 +1,22 @@
+def pointScore():
+    pass
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    f = open("Python/Day 4/input.txt", "r")
+    lines = f.readlines()
+    part1(lines)
+    part2(lines)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
