f4e8c589ed378d21394ca4481a19965189ac299b
Matthew <matthewcurtis@uvic.ca>
2022-12-05 01:58:56 -0800

    Cleaned up solution

--- Python/Day 5/main.py
+++ Python/Day 5/main.py
@@ -1,69 +1,72 @@
 import re
 
 
-def clean(lst):
-    lst.append(pop(lst))
-    return lst
-
-
 def pop(cargo):
     x = cargo.pop()
-    match = re.search(r"\[[A-Z]\]", x)
+    match = re.search(r"[A-Z]", x)
     if match:
         return x
     else:
         return pop(cargo)
 
 
-def move(num, frm, to, cargo):
-    # print("move", num, "from", frm, "to", to)
-    lst = []
-    for i in range(num):
-        lst.append(cargo[frm - 1].pop())
+def parseCrates(crates):
+    crates.reverse()
+    cargo = []
+    for i in range(len(crates) + 1):
+        x = list(list(zip(*crates))[0])
+        for stack in crates:
+            if stack != []:
+                stack.remove(stack[0])
+            if stack == []:
+                stack.append(" ")
+        cargo.append(x)
+        for x in cargo:
+            x.append(pop(x))
+    return cargo
 
-    for i in range(num):
-        cargo[to - 1].append(lst.pop())
+
+def move(num, frm, to, crates, part):
+
+    if part == 1:
+        for i in range(num):
+            crates[to - 1].append(crates[frm - 1].pop())
+    if part == 2:
+        lst = []
+        for i in range(num):
+            lst.append(crates[frm - 1].pop())
+        for i in range(num):
+            crates[to - 1].append(lst.pop())
+
+
+def printTop(crates):
+    for crate in crates:
+        print(crate[-1], end="")
 
 
 def main():
     f = open("Python/Day 5/input.txt", "r")
     lines = f.readlines()
-    part1Solution = 0
-    part2Solution = 0
-    i = 0
-    stacks = []
-    cargo = []
-    drawingFlag = False
+    instructionsFlag = False
+    startingCrates = re.compile(r"\[[(A-Z)]\]|\s{4}")
+    instructions = re.compile(r"move (\d+) from (\d+) to (\d+)")
+    crates = []
     for line in lines:
-        match = re.findall(r"\[[A-Z]\]|\s\d\d?\s\s?|\s{3}[\s]", line)
-        if match and not drawingFlag:
-            # print("unchanged", line)
-            match = re.findall(r"\[[A-Z]\]|\s\d\d?\s\s?|\s{3}[\s]", line)
-            stacks.append(match)
-        # print("changed", match)
+        if line[0] == " ":
+            instructionsFlag = True
+            crates = parseCrates(crates)
 
-        if match == []:
-            numberOfStacks = len(stacks.pop())
-            stacks.reverse()
-            drawingFlag = True
-            for i in range(numberOfStacks):
-                x = list(list(zip(*stacks))[0])
-                for stack in stacks:
-                    if stack != []:
-                        stack.remove(stack[0])
-                    if stack == []:
-                        stack.append(" ")
-                cargo.append(x)
-            for x in cargo:
-                x = clean(x)
-                print("before", x)
-            continue
-        if drawingFlag:
-            match = [int(x.strip()) for x in match]
-            # print(match)
-            move(match[0], match[1], match[2], cargo)
-    for x in cargo:
-        print(x.pop())
+        if not instructionsFlag:
+            match = startingCrates.findall(line)
+            if match:
+                crates.append(match)
+
+        if instructionsFlag:
+            match = instructions.match(line)
+            if match:
+                match = (int(match[1]), int(match[2]), int(match[3]))
+                move(*match, crates, 1)
+    printTop(crates)
     f.close()
 
 
